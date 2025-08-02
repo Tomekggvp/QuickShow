c370db1c6ddb990d606b40e0a97651d995e2c112
Tomekggvp <tomek2005@mail.ru>
2025-08-02 19:09:27 +0300

    1.0.0

---
+++ src/components/TrailerSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { dummyTrailers } from '../assets/assets';
+import ReactPlayer from 'react-player';
+import BlurCircle from './BlurCircle'
+
+const TrailerSection = () => {
+
+const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0])
+
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
+      <p className='text-gray-300 font-medium text-lg max-w-[960px]'>
+        Trailers
+      </p>
+
+    <div>
+        <BlurCircle />
+        <ReactPlayer url={currentTrailer.videoUrl} controls={false} 
+        className='mx-auto max-w-full' width="960px" height="540px"/>
+    </div>
+
+    </div>
+  );
+};
+
+export default TrailerSection
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+import FeaturedSection from '../components/FeaturedSection';
+import MovieCard from '../components/MovieCard';
+
+
+const Home = () => {
+  return <>
+  
+  <HeroSection/>
+  <FeaturedSection/>
+  <MovieCard/>
+  </>;
+};
+
+export default Home
