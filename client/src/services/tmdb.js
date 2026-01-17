01f6e53a8d522b7c46f7f93b74399753feeefa09
Tomekggvp <tomek2005@mail.ru>
2026-01-18 02:49:03 +0300

    fix proxy settings 5.0

--- client/src/services/tmdb.js
+++ client/src/services/tmdb.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
 const tmdb = axios.create({
-  // Это публичное зеркало (прокси), оно работает без VPN и лишних настроек
-  baseURL: 'https://tmdb-proxy.movies-api.workers.dev/3', 
+  baseURL: 'https://corsproxy.io/?https://api.themoviedb.org/3',
   params: {
-    // Вставьте сюда ваш ключ напрямую для теста
     api_key: 'f53964ee2cf1b5f1f14af077bbff023e', 
     language: 'en-US',
   },
+});
+
+// Добавим проверку в консоль, чтобы видеть, какой URL формируется
+tmdb.interceptors.request.use(config => {
+  console.log('Final URL:', config.baseURL + config.url);
+  return config;
 });
 
 export default tmdb;
