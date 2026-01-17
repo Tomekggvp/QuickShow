e5e4dd80e369f87ed0cdeeb5c22460adf4ac632e
Tomekggvp <tomek2005@mail.ru>
2026-01-18 02:53:23 +0300

    fix proxy settings 6.0

--- client/src/services/tmdb.js
+++ client/src/services/tmdb.js
@@ -1,17 +1,28 @@
 import axios from 'axios';
 
 const tmdb = axios.create({
-  baseURL: 'https://corsproxy.io/?https://api.themoviedb.org/3',
-  params: {
-    api_key: 'f53964ee2cf1b5f1f14af077bbff023e', 
-    language: 'en-US',
-  },
+
+  baseURL: 'https://api.allorigins.win/get?url=' + encodeURIComponent('https://api.themoviedb.org/3'),
 });
 
-// Добавим проверку в консоль, чтобы видеть, какой URL формируется
-tmdb.interceptors.request.use(config => {
-  console.log('Final URL:', config.baseURL + config.url);
-  return config;
+tmdb.interceptors.response.use((response) => {
+
+  if (response.data && response.data.contents) {
+    return {
+      ...response,
+      data: JSON.parse(response.data.contents)
+    };
+  }
+  return response;
+});
+
+
+export const getParams = (extraParams = {}) => ({
+  params: {
+    api_key: 'f53964ee2cf1b5f1f14af077bbff023e',
+    language: 'en-US',
+    ...extraParams
+  }
 });
 
 export default tmdb;
